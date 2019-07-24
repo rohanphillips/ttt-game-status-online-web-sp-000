97feb43a763332034b86b9bca25e768e3993f022
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-24 18:50:34 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,7 +21,7 @@ def won?(board)
   if winner(board) !== true 
     return false  
   else 
-    if winningcombo = 0 
+    if winningcombo == 0 
       return WIN_COMBINATIONS[winningcombo]
     end
   end
